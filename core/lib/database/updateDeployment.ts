b28005d5825f3ccfbf7f47bb6cb1723b60955328
Ilia <solohin.i@gmail.com>
2021-06-26 01:16:48 +0200

    #35  do not redeploy system deployments on start

--- core/lib/database/updateDeployment.ts
+++ core/lib/database/updateDeployment.ts
@@ -1,15 +1,24 @@
 import safePatch from "./safePatch"
 import { DeploymentUpdate } from "../../definitions"
+import deepEqual from "deep-equal"
 
-export default async function updateDeployment(validatedBody: DeploymentUpdate) {
-    await safePatch(`deployments/${validatedBody.name}`, (oldDeployment): object => {
+
+export default async function updateDeployment(deploymentUpdate: DeploymentUpdate) {
+    await safePatch(`deployments/${deploymentUpdate.name}`, (oldDeployment): object => {
         //TODO: update only if config changed
-        const needNewPods = true
+
+        console.log('oldDeployment.currentConfig', oldDeployment.currentConfig)
+        console.log('deploymentUpdate', deploymentUpdate)
+
+        const needNewPods = !deepEqual(//compare configs without scale
+            Object.assign({}, oldDeployment.currentConfig, { scale: -1 }),
+            Object.assign({}, deploymentUpdate, { scale: -1 }),
+        )
         if (needNewPods) {
             oldDeployment.currentPodNames = []
         }
 
-        oldDeployment.currentConfig = validatedBody
+        oldDeployment.currentConfig = deploymentUpdate
 
         return oldDeployment
     })
