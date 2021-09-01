72b6c58f21a7fabdfa1dd0fcbba0b34587120433
Ilia <solohin.i@gmail.com>
2021-09-01 19:50:19 +0200

    #69 wip

--- src/cluster-setup/index.ts
+++ src/cluster-setup/index.ts
@@ -1,11 +1,25 @@
 import stackDeploy from "../lib/docker/stackDeploy";
 import fs from "fs";
 import path from "path";
+import axios from "axios";
+import mustache from "mustache";
+import config from "../config"
+import logger from "../lib/logger"
 
 async function start() {
-    const yaml = fs.readFileSync(path.join(__dirname, "../..", "cluster-setup/cluster-config.yaml"), "utf8");
-    await stackDeploy(yaml, 'containeros-system')
+    const templateYaml = fs.readFileSync(path.join(__dirname, "../..", "cluster-setup/cluster-config.yaml"), "utf8");
+    const resultYaml = mustache.render(templateYaml, config, {}, ['<%', '%>'])
+    logger.debug(resultYaml)
+    await stackDeploy(resultYaml, 'containeros-system')
 }
+
+async function getMyExternalIP() {
+    const res = await axios.get('https://api.ipify.org?format=json')
+    return res.data.ip
+}
+
+//TODO: create caddy net
+// docker network create -d overlay --attachable caddy
 
 start().catch(e => {
     console.error(e)
--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
 const config = {
-    ENV: 'dev',
+    ENV: process.env.ENV || 'dev',
     CLUSTER_API_PORT: 8080,
     MAX_APP_SCALING: 10,
     IS_TEST: undefined,
     IS_DEV: undefined,
     IS_PROD: undefined,
-    CONSUL_HOST: undefined
+    CONSUL_HOST: undefined,
+    REGISTRY_STORAGE_S3_ACCESSKEY: process.env.REGISTRY_STORAGE_S3_ACCESSKEY,
+    REGISTRY_STORAGE_S3_SECRETKEY: process.env.REGISTRY_STORAGE_S3_SECRETKEY,
+    REGISTRY_STORAGE_S3_BUCKET: process.env.REGISTRY_STORAGE_S3_BUCKET,
+    REGISTRY_STORAGE_S3_REGIONENDPOINT: process.env.REGISTRY_STORAGE_S3_REGIONENDPOINT,
+    REGISTRY_STORAGE_S3_REGION: process.env.REGISTRY_STORAGE_S3_REGION,
 }
 
 if (process.env.ENV === "production") {
@@ -28,14 +36,16 @@ if (config.IS_DEV) {
     config.CONSUL_HOST = 'consul1'
 }
 
-
 var proxy = new Proxy(
     config,
     {
         get: function (obj, name) {
-            if (typeof obj[name] === undefined) {
-                throw Error(`Trying to get ${String(name)} config value, which is not initialized(yet)`)
+            if (typeof obj[name] === "undefined") {
+                const logger = require("./lib/logger").default
+                logger.fatal("undefined config value: " + String(name) + ". Avalable values: ", Object.keys(obj).join(", "))
+                process.exit(1)
             }
+
             return obj[name];
         },
     }
