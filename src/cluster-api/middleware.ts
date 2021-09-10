03251c1c1df1a3d5aeac6b69b2fb147e89eb6d1a
Ilia <solohin.i@gmail.com>
2021-09-10 18:03:04 +0200

    #77 fix API crash issue

--- src/cluster-api/middleware.ts
+++ src/cluster-api/middleware.ts
@@ -7,6 +7,7 @@ import { HttpCodes, HttpError } from "../lib/http/Error";
 import { sha256 } from "../lib/utils";
 import { StoredUser } from "../types";
 import config from "../config"
+import logger from "../lib/logger";
 
 export default {
     init(app: express.Application) {
@@ -31,19 +32,26 @@ export default {
 
                 console.log({
                     'req.headers.authorization': req.headers.authorization,
-                    url: req.url, login, password, user
+                    url: req.url, login, password, user, 'ROOT_TOKEN (debug, remove it)': config.ROOT_TOKEN
                 })
 
                 //root allowed everything
-                if (login === 'root' && password === config.ROOT_TOKEN) {
-                    return next();
+                if (login === 'root') {
+                    if (password === config.ROOT_TOKEN) {
+                        return next();
+                    } else {
+                        logger.info(`Wrong root token`)
+                        return next(new HttpError(HttpCodes.Unauthorized, `Wrong root token`))
+                    }
                 }
                 //sorry user not found
                 if (user === null) {
+                    logger.info(`User ${login} not found`)
                     return next(new HttpError(HttpCodes.Unauthorized, `Unknown user "${login}"`))
                 }
                 //check password
                 if (sha256(password) !== user.tokenHash) {
+                    logger.info(`Wrong password or token for user "${login}"`)
                     return next(new HttpError(HttpCodes.Unauthorized, `Wrong password or token for user "${login}"`))
                 }
                 //docker registry auth request
