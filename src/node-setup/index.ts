9123ab3a83059261a4f70be7602d1b0446c42837
Ilia <solohin.i@gmail.com>
2021-09-12 12:37:41 +0200

    #77 make problem

--- src/node-setup/index.ts
+++ src/node-setup/index.ts
@@ -1,7 +1,10 @@
 import config from "../config"
 import execa from "execa"
+import logger from "../lib/logger"
 
-async function start() {
+let setupComplete = false
+async function setup() {
+    if (setupComplete) return
     await execa('docker', [
         'login',
         config.API_HOST,
@@ -9,6 +12,24 @@ async function start() {
         '-p', config.ROOT_TOKEN,
     ])
     console.log(`Node set up complete`)
+    setupComplete = true
+}
+
+async function start() {
+    for (let i = 0; i < 5; i++) {
+        try {
+            await setup()
+            logger.info(`Set up complete`)
+            break
+        } catch (e) {
+            logger.debug(e)
+            await new Promise(resolve => setTimeout(resolve, 1000))
+        }
+    }
+
+    await setup()
+
+    setInterval(() => console.log(`Keeping this container alive`), 60 * 60 * 1000)
 }
 
 start().catch(e => {
