da4ce25cab01f66e4c8dace076c6be4d1bfa591c
Ilia S <solohin.i@gmail.com>
2021-10-30 13:23:02 -0400

    research: lvcreate

--- cos-sds-dev/dockerize/lvcreate.sh
+++ cos-sds-dev/dockerize/lvcreate.sh
@@ -1,13 +1,9 @@
 
 docker rm -f lvm;
-docker run -it --name lvm  --pid=host \
-    -v /bin:/bin \
+docker run -it --name lvm  \
     -v /usr:/usr \
-    -v /etc:/etc \
-    -v /run:/run \
-    -v /dev:/dev \
     --privileged \
     ubuntu:focal \
-    bash -c "lvs; timeout 3 lvcreate -V1G -T vg/lvmthinpool -y -n r8; lvs"
+    bash -c "lvs; lvcreate -V1G -T vg/lvmthinpool -y -n testvol; lvs; lvremove vg/testvol -y; lvs;"
 
 
